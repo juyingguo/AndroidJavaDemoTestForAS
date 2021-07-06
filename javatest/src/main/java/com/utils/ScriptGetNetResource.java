d8cca1d48cee9bb005b4ab755d5c76bae1af1092
juyingguo <juying2050@sina.cn>
2021-07-06 14:01:35 +0800

    javatest:add java script test

---
+++ javatest/src/main/java/com/utils/ScriptGetNetResource.java
@@ -0,0 +1,14 @@
+package com.utils;
+
+/**
+ * Date:2021/6/30,14:21
+ * author:jy
+ * <p>脚本：用于获取资源</p>
+ */
+public class ScriptGetNetResource {
+    public static void main(String[] args) {
+        //动物类别：规则:用一个字符串形式配置所有资源，字符串内每一个字符以&分割
+        //老虎&大象&河马
+
+    }
+}
