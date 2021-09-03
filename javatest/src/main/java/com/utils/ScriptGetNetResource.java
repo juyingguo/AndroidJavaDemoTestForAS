8d4889df367f61a2f36f3e8fcc50a8e9900b1dca
juyingguo <juying2050@sina.cn>
2021-09-03 15:04:41 +0800

    javatest:ScriptGetNetResource.java add animal

--- javatest/src/main/java/com/utils/ScriptGetNetResource.java
+++ javatest/src/main/java/com/utils/ScriptGetNetResource.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 
@@ -28,33 +29,45 @@ import okhttp3.Request;
 /**
  * Date:2021/6/30,14:21
  * author:jy
- * <p>脚本：用于获取资源</p>
+ * <p>脚本：用于获取资源
+ * <p>1.动物名称不同，豆豆返回的json结构不同，即：stage的jsonObject不同，有的文本图像音频在一个jsonObject中，有的分布在不同的jsonObject中。
+ * <p>1.1 狮子,result:{"stage":[{"image":"http://resource.doudoubot.cn/download/image/domain/animal/animal_24.jpg","message":"狮子的体型大，躯体均匀，四肢中长。","url":"http://resource.doudoubot.cn/download/audio/domain/animal/shizi_24.mp3"}],"status":0}
+ * <p>1.2大象,result:{"stage":[{"message":"小i唱一首《大象》，要掌声鼓励哦！"},{"image":"http://resource.doudoubot.cn/download/image/play/play_song.jpg","url":"http://resource.doudoubot.cn/download/audio/play/01-daxiang.mp3"}],"status":0}
+ * <p>1.3存在问题,举例如狮子：图片名称例如：animal_24.jpg，而音乐为：shizi_24.mp3；为了便于查看，此时需要将图片名称改为和音乐名称一致。
+ * <p>1.4保留原始图片地址，便于下载用，json字段命名为rawImageUrl；
+ * <p>1.5使用map,动物名称的拼音作为key,观察结果，可用音频不带后缀的文件名作为key,图片和音频地址组合成集合作为value;待图片下载时，将key + 原图片后缀名共同作为图片下载后的本地存储文件名。
+ * <p>1.5.1 有些只有图片，不好统一处理，暂且不处理了。
  */
 public class ScriptGetNetResource {
     private static String TAG = "ScriptGetNetResource";
+    //，,
     public static String[] animalArray = new String[]{
-            ////动物类 、
-            "老虎","狮子","大象"/*,"狼","老鼠","长颈鹿","大象","貂","猴子","斑马","狗","狐狸","斑马"
-            ,"熊","豹子","熊猫","大熊猫","羚羊","驯鹿","考拉","犀牛","袋鼠","穿山甲","河马","猩猩"
-            ,"海牛","水獭","海豹","海豚","海象","鸭嘴兽","蝙蝠","","",""
-            ,"刺猬","北极熊","鲸鱼","兔子","小白兔","黄鼠狼","","","",""
-            ///家禽家畜
-            ,"猪","牛","羊","马","骆驼","兔子","猫","狗","驴","骡子","","","",""
-            ,"鸡","公鸡","母鸡","小鸡","鸭子","鹅","火鸡","鸽子","鹌鹑","","","",""
+            //动物综合分类： 宠物 哺乳 鸟类 鱼类 爬行 两栖 昆虫 无脊
+
+            ////哺乳动物类
+            "老虎","狮子","大象","狼","老鼠","长颈鹿","驯鹿","梅花鹿","大象","貂","猴子","狒狒","斑马","狗","狐狸","斑马"
+            ,"熊","熊猫","浣熊","豹子","羚羊","考拉","犀牛","袋鼠","穿山甲","河马","猩猩",""
+            ,"海牛","水獭","海豹","海豚","海象","鸭嘴兽","蝙蝠","蚂蚁","山羊","松鼠"
+            ,"刺猬","北极熊","鲸鱼","虎鲸","逆戟鲸","小白兔","黄鼠狼","兔子","树懒","",""
+            ,"猪","牛","水牛","羊","马","骆驼","兔子","猫","狗","藏獒","驴","骡子","","",""
             //鱼类
-            ,"鲨鱼","章鱼","水獭","鳄鱼","鲈鱼","鲤鱼","金枪鱼","鲟鱼","水獭"
+            ,"鲨鱼","章鱼","鳄鱼","鲈鱼","鲤鱼","金枪鱼","鲟鱼",""
             ///鸟类
-            ,"老鹰","鹰","鹅","企鹅","鹦鹉","啄木鸟","鸵鸟","翠鸟","天鹅","蜂鸟","信天翁","鹤","夜莺","海鸥","夜莺"
+            ,"老鹰","鹰","鹅","企鹅","鹦鹉","啄木鸟","鸵鸟","翠鸟","天鹅","蜂鸟","信天翁","鹤","夜莺","海鸥","孔雀","","","",""
+            ,"鸡","公鸡","母鸡","小鸡","鸭子","鹅","火鸡","鸽子","鹌鹑","","","",""
+            ///爬行
+
             ///两栖动物
             ,"海狮","乌龟","蜥蜴","蟾蜍","青蛙","蛇","",""
             ///昆虫  肉足虫 藤壶
             ,"蝴蝶","蜻蜓","蝎子","珊瑚","纤毛虫","绦虫","","吸虫","水蚤","","蟋蟀","蜈蚣","蝗虫","","","","",""
             ///其他动物  草履虫
-            ,"蚯蚓","知了","蝉","恐龙","海蜇","海参","海绵","水母","水螅","海星","乌贼","海葵","海胆","","","","",""*/
+            ,"蚯蚓","知了","蝉","恐龙","海蜇","海参","海绵","水母","水螅","海星","乌贼","海葵","海胆","","","","",""
     };
     private static String nlpDouDouUrlPrefix = "http://api.doudoubot.cn/rsvpbot/general/chat?appid=rsvpupR18lm6q8i0&token=HD8Mn045gGzZ8foc&userid=123456&question=";
-    private static LinkedList<String> mImageList = new LinkedList<String>();
-    private static LinkedList<String> mAudioList = new LinkedList<String>();
+    private static LinkedList<String> mImageList = new LinkedList<>();
+    private static LinkedList<String> mAudioList = new LinkedList<>();
+    //private static HashMap<String,LinkedList<String>> storeToDownloadMediaResMap = new HashMap<>();
     public static void main(String[] args) {
         new Thread(new Runnable() {
             @Override
@@ -63,53 +76,6 @@ public class ScriptGetNetResource {
                 downloadMediaResource();
             }
         }).start();
-    }
-
-    private static void downloadMediaResource() {
-        System.out.println(TAG + "downloadMediaResource enter.");
-        String url = null, fileDir = null, fileName = null;
-        boolean isImage = true;
-        if (!mImageList.isEmpty()){
-            url = mImageList.removeFirst();
-            isImage = true;
-        }else {
-            if (!mAudioList.isEmpty()){
-                url = mAudioList.removeFirst();
-                isImage = false;
-            }
-        }
-        if (url == null || url.equals("")) {
-            System.out.println(TAG + "downloadMediaResource url is null,all finish.");
-            return;
-        }
-
-        if (isImage){
-            fileDir = "D:/Download/animal/image";
-        }else {
-            fileDir = "D:/Download/animal/audio";
-        }
-        fileName = FileUtils.getFileNameByHttpUrl(url);
-        System.out.println(TAG + "downloadMediaResource fileName:" + fileName);
-        OkHttpUtils.get().url(url).build().execute(new FileCallBack(fileDir,fileName) {
-            @Override
-            public void onBefore(Request request, int id) {
-                super.onBefore(request, id);
-                System.out.println(TAG + "onBefore>>id:" + id);
-                downloadMediaResource();
-            }
-
-            @Override
-            public void onError(Call call, Exception e, int i) {
-                System.out.println(TAG + "onError>>e:" + e);
-                downloadMediaResource();
-            }
-
-            @Override
-            public void onResponse(File file, int i) {
-                System.out.println(TAG + "onResponse>>file:" + file);
-                downloadMediaResource();
-            }
-        });
     }
 
     private static void getResources() {
@@ -136,10 +102,6 @@ public class ScriptGetNetResource {
             JsonElement parse = jsonObject.parse(result);
             JsonObject asJsonObject = parse.getAsJsonObject();
             int status = asJsonObject.get("status").getAsInt();
-            ///动物名称不同，豆豆返回的json结构不同，即：stage的jsonObject不同，有的文本图像音频在一个jsonObject中，有的分布在不同的jsonObject中。
-            ///狮子,result:{"stage":[{"image":"http://resource.doudoubot.cn/download/image/domain/animal/animal_24.jpg","message":"狮子的体型大，躯体均匀，四肢中长。","url":"http://resource.doudoubot.cn/download/audio/domain/animal/shizi_24.mp3"}],"status":0}
-            ///大象,result:{"stage":[{"message":"小i唱一首《大象》，要掌声鼓励哦！"},{"image":"http://resource.doudoubot.cn/download/image/play/play_song.jpg","url":"http://resource.doudoubot.cn/download/audio/play/01-daxiang.mp3"}],"status":0}
-            ///存在问题：图片名称例如：animal_24.jpg，而音乐为：shizi_24.mp3；为了便于查看，此时将图片名称改为和音乐名称一致。
             if (status == 0){
                 JsonArray stageArray = asJsonObject.getAsJsonArray("stage");
                 JsonObject jsonBean = null;
@@ -160,9 +122,14 @@ public class ScriptGetNetResource {
                     }
                 }
                 /*//此时将图片名称改为和音乐名称一致，后缀保持原样
-                JsonElement audioUrl = outJson.get("audioUrl");
-                JsonElement imageUrl = outJson.get("imageUrl");
-                System.out.println("getResources(),audioUrl:" + audioUrl);*/
+                if (outJson.has("audioUrl")){
+                    JsonElement audioUrl = outJson.get("audioUrl");
+                    System.out.println("getResources(),audioUrl:" + audioUrl);
+                }
+
+                FileUtils.getFileNameNoExtensionByHttpUrl()
+                JsonElement imageUrl = outJson.get("imageUrl");*/ ///这种情况先不处理了。
+
                 refactorOutStr = "..JSON_IBOTN_IFLYTEK_"+ outJson.toString();
                 System.out.println("getResources(),animal:" + animalArray[i] + ",refactorOutStr:" + refactorOutStr);
                 hashMap.put(animalArray[i],refactorOutStr);
@@ -244,6 +211,53 @@ public class ScriptGetNetResource {
         workbook.close();
         os.close();
         System.out.println(TAG + ",createExcel ok." );
+    }
+
+
+    private static void downloadMediaResource() {
+        System.out.println(TAG + ",downloadMediaResource enter.");
+        String url = null, fileDir = null, fileName = null;
+        boolean isImage = true;
+        if (!mImageList.isEmpty()){
+            url = mImageList.removeFirst();
+            isImage = true;
+        }else {
+            if (!mAudioList.isEmpty()){
+                url = mAudioList.removeFirst();
+                isImage = false;
+            }
+        }
+        if (url == null || url.equals("")) {
+            System.out.println(TAG + ",downloadMediaResource url is null,all finish.");
+            return;
+        }
+
+        if (isImage){
+            fileDir = "D:/Download/animal/image";
+        }else {
+            fileDir = "D:/Download/animal/audio";
+        }
+        fileName = FileUtils.getFileNameByHttpUrl(url);
+        System.out.println(TAG + ",downloadMediaResource fileName:" + fileName);
+        OkHttpUtils.get().url(url).build().execute(new FileCallBack(fileDir,fileName) {
+            @Override
+            public void onBefore(Request request, int id) {
+                super.onBefore(request, id);
+                System.out.println(TAG + ",onBefore>>id:" + id);
+            }
+
+            @Override
+            public void onError(Call call, Exception e, int i) {
+                System.out.println(TAG + ",onError>>e:" + e);
+                downloadMediaResource();
+            }
+
+            @Override
+            public void onResponse(File file, int i) {
+                System.out.println(TAG + ",onResponse>>file:" + file);
+                downloadMediaResource();
+            }
+        });
     }
 
 }
