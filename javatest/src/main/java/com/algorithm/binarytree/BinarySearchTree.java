ef87f72169511fa14a89fe8fc71dab1f02103eb9
juyingguo <juying2050@sina.cn>
2021-12-07 17:09:12 +0800

    add binarytree

---
+++ javatest/src/main/java/com/algorithm/binarytree/BinarySearchTree.java
@@ -0,0 +1,120 @@
+package com.algorithm.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://blog.csdn.net/z_yemu/article/details/106678716
+ */
+public class BinarySearchTree {
+    private Node tree;
+    public Node getTree() {
+        return tree;
+    }
+
+    public void setTree(Node tree) {
+        this.tree = tree;
+    }
+
+    /**
+     * 先取根节点，如果它等于我们要查找的数据，那就返回。
+     * 如果要查找的数据比根节点的值小，那就在左子树中递归查找；如果要查找的数据比根节点的值大，那就在右子树中递归查找。
+     * @param data data
+     * @return Node
+     */
+    public Node find(int data) {
+        Node p = tree;
+        while (p != null) {
+            if (data < p.data) p = p.left;
+            else if (data > p.data) p = p.right;
+            else return p;
+        }
+        return null;
+    }
+
+    /**
+     * 如果要插入的数据比节点的数据大，并且节点的右子树为空，就将新数据直接插到右子节点的位置；如果不为空，就再递归遍历右子树，查找插入位置。
+     * 同理，如果要插入的数据比节点数值小，并且节点的左子树为空，就将新数据插入到左子节点的位置；如果不为空，就再递归遍历左子树，查找插入位置。
+     * @param data
+     */
+    public void insert(int data) {
+        if (tree == null) {
+            tree = new Node(data);
+            return;
+        }
+
+        Node p = tree;
+        while (p != null) {
+            if (data > p.data) {
+                if (p.right == null) {
+                    p.right = new Node(data);
+                    return;
+                }
+                p = p.right;
+            } else { // data < p.data
+                if (p.left == null) {
+                    p.left = new Node(data);
+                    return;
+                }
+                p = p.left;
+            }
+        }
+    }
+    /**
+     * 二叉树同层遍历
+     * @param root root TreeNode
+     * @return List<TreeNode>
+     */
+    List<Node> floorLevelTree(Node root){
+        List<Node> result = new ArrayList<>();
+        Node node = root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+        while (!queue.isEmpty()){
+            node = queue.poll();
+            result.add(node);
+            if (node.left != null){
+                queue.offer(node.left);
+            }
+            if (node.right != null){
+                queue.offer(node.right);
+            }
+        }
+        return result;
+    }
+    public static class Node {
+        private int data;
+        private Node left;
+        private Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+    }
+}
