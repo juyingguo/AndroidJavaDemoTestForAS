a5164c17832a5c9221c958b3bffbd1f99ef791b2
juyingguo <juying2050@sina.cn>
2021-12-10 17:56:53 +0800

    renameWithReplaceNameFile

--- javatest/src/main/java/com/file/FileRenameTest.java
+++ javatest/src/main/java/com/file/FileRenameTest.java
@@ -14,11 +14,12 @@ import java.util.List;
  */
 public class FileRenameTest {
     public static void main(String[] args) {
-        renameFile();        
+//        renameWithTrimPrefixFile();
+        renameWithReplaceNameFile();
     }
-    private static String ROOT_DIR = "D:\\juying\\as1_5project\\ibotnsvn\\ibotncourse\\IbotnIzlCourseNT_as\\izlcoursezbnt\\src\\main\\assets\\web\\IbotnCourse_zb\\view\\2B_18_StatisticalTable";
+    private static String ROOT_DIR = "D:\\juying\\work-test\\game-course\\html5\\izhile_xiaxueqi\\temp";
 //    private static String ROOT_DIR = "D:\\juying\\as1_5project\\ibotnsvn\\ibotncourse\\IbotnIzlCourseNT_as\\izlcoursezbnt\\src\\main\\assets\\web\\IbotnCourse_zb\\assets\\images";
-    private static void renameFile() {
+    private static void renameWithTrimPrefixFile() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -37,9 +38,39 @@ public class FileRenameTest {
                         String name = FileUtils.getFileName(file);
                         if (name.startsWith("test_")){
                             String newName = name.substring("test_".length());
-                            System.out.println("renameFile(),find file [ file name start with test_ ],rename to no test_ prefix,old name:" + name);
+                            System.out.println("renameWithTrimPrefixFile(),find file [ file name start with test_ ],rename to no test_ prefix,old name:" + name);
                             file.renameTo(new File(file.getParentFile(),newName));
-//                            System.out.println("renameFile(),file new name file.getName():" + file.getName());
+//                            System.out.println("renameWithTrimPrefixFile(),file new name file.getName():" + file.getName());
+
+                        }
+
+                    }
+                }
+            }
+        }).start();
+    }
+    private static void renameWithReplaceNameFile() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (FileUtils.isDir(ROOT_DIR)){
+                    List<File> files = FileUtils.listFilesInDirWithFilter(ROOT_DIR, new FilenameFilter() {
+                        @Override
+                        public boolean accept(File dir, String name) {
+                            if (FileUtils.isImage(new File(dir, name).getAbsolutePath())
+                                    || FileUtils.isAudio(new File(dir, name).getAbsolutePath())) {
+                                return true;
+                            }
+                            return false;
+                        }
+                    });
+                    for (File file:files) {
+                        String name = FileUtils.getFileName(file);
+                        if (name.startsWith("second_")){
+                            String newName = "eighth_" + name.substring("second_".length());
+                            System.out.println("renameWithReplaceNameFile(),find file [ file name start with test_ ],rename to no test_ prefix,old name:" + name);
+                            file.renameTo(new File(file.getParentFile(),newName));
+//                            System.out.println("renameWithTrimPrefixFile(),file new name file.getName():" + file.getName());
 
                         }
 
