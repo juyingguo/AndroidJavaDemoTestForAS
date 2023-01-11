a2f70e7c5ac341828794635c90eb05c9757a0e00
juying-eink <juying2050@sina.cn>
2023-01-11 16:03:37 +0800

    move module advancelight_chapter_10_mvpsimple to MoonMvpSimple  *\as-some-sample-project-to-github\android-books-code-debug\android-advanced-light\

--- advancelight_chapter_10_mvpsimple/src/main/java/com/example/liuwangshu/moonmvpsimple/LoadTasksCallBack.java
+++
@@ -1,8 +0,0 @@
-package com.example.liuwangshu.moonmvpsimple;
-
-public interface LoadTasksCallBack<T> {
-    void onSuccess(T t);
-    void onStart();
-    void onFailed();
-    void onFinish();
-}
